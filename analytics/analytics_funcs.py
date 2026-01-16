70218fd9861b2cea9672474eb242f2a53ebb938a
Samuel <sojonesey@gmail.com>
2026-01-16 00:38:46 +0000

    Remove unnecessary session state assignments in top_songs, top_artists, and top_albums functions

--- analytics/analytics_funcs.py
+++ analytics/analytics_funcs.py
@@ -312,7 +312,6 @@ def top_songs(df, show_uri=True, config=None):
         }
     }
     
-    st.session_state.song_sum = song_sum
     top_results = get_top_n(song_sum, sort_configs, n=config.top_n, min_plays_filter=True)
     
     return TopResults(
@@ -363,7 +362,6 @@ def top_artists(df, config=None):
         }
     }
     
-    st.session_state.artist_sum = artist_sum
     top_results = get_top_n(artist_sum, sort_configs, n=config.top_n, min_plays_filter=True)
     
     return TopArtistResults(
@@ -422,7 +420,6 @@ def top_albums(df, config=None, single=False):
             }
         }
 
-        st.session_state.album_sum = album_sum
         top_results = get_top_n(album_sum, sort_configs, n=Config.top_n, min_plays_filter=True)
 
     return TopArtistResults(
